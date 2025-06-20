4443b3486fc19b5c88e4d7d3f4e2c5a0f0373674
Samoreilly <x23756575@student.ncirl.ie>
2025-06-20 16:09:35 +0100

    Configured RedisConfiguration, KafkaConsumer and KafkaProducer, to take in transactions

--- src/main/java/fraud/fraud/FraudDetectorApplication.java
+++ src/main/java/fraud/fraud/FraudDetectorApplication.java
@@ -2,8 +2,10 @@ package fraud.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FraudDetectorApplication {
 
 	public static void main(String[] args) {
