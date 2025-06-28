9c925f7117d2b3ceeb4206b97995b3492033f3d7
Samoreilly <x23756575@student.ncirl.ie>
2025-06-28 16:55:21 +0100

    Setup up google oauth to associate users accounts with there transactions and not random JSession ids

---
+++ src/main/java/fraud/fraud/entitys/User.java
@@ -0,0 +1,26 @@
+package fraud.fraud.entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public User(){
+
+    }
+    public User(Long id) {
+        this.id = id;
+    }
+}
