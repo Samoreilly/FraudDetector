d8c3d3e16f8fc8d366f1743812ebba3218764cb5
Samoreilly <x23756575@student.ncirl.ie>
2025-06-29 14:03:34 +0100

    Removed http session everywhere and used @AuthenticationPrinciap to intercept google "sub"

--- src/main/java/fraud/fraud/controllers/TransactionController.java
+++ src/main/java/fraud/fraud/controllers/TransactionController.java
@@ -9,7 +9,9 @@ import fraud.fraud.services.SetupSse;
 import fraud.fraud.services.TransactionSecurityCheck;
 import fraud.fraud.services.TransactionService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,69 +24,54 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class TransactionController {
 
-    private final TransactionService transactionService;
+    //next up connect user to database, set session id from user id in database, pass to frontend and use in redis caching and kafka send()
     private final RateLimiting rateLimiting;
     private final KafkaTemplate<String, TransactionRequest> kafkaTemplate;
     private final SetupSse setupSse;
     private final CustomMetricsService customMetricsService;
     private final TransactionSecurityCheck transactionSecurityCheck;
 
-    public TransactionController(TransactionService transactionService, RateLimiting rateLimiting, KafkaTemplate<String, TransactionRequest> kafkaTemplate, SetupSse setupSse, CustomMetricsService customMetricsService, TransactionSecurityCheck transactionSecurityCheck) {
-        this.transactionService = transactionService;
+    public TransactionController(RateLimiting rateLimiting, KafkaTemplate<String, TransactionRequest> kafkaTemplate, SetupSse setupSse, CustomMetricsService customMetricsService, TransactionSecurityCheck transactionSecurityCheck) {
         this.rateLimiting = rateLimiting;
         this.kafkaTemplate = kafkaTemplate;
         this.setupSse = setupSse;
         this.customMetricsService = customMetricsService;
         this.transactionSecurityCheck = transactionSecurityCheck;
     }
-//    @PostMapping("/")
-//    public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
-//
-//        if(principal == null){
-//            System.out.println("PRINCIPAL NULL");
-//            return Map.of("error", "PRINCIPAL NULL");
-//        }
-//        System.out.println("GOOGLE DETAILS");
-//        System.out.println(principal.getAttributes());
-//        return principal.getAttributes();
-//    }
+
 
     @PostMapping("/tr")
-    public ResponseEntity<String> transaction(@RequestBody TransactionRequest transactionInfo, HttpServletRequest request, HttpSession session, @AuthenticationPrincipal OAuth2User principal) throws IOException {
+    public ResponseEntity<String> transaction(@RequestBody TransactionRequest transactionInfo, HttpServletRequest request, @AuthenticationPrincipal OAuth2User principal) throws IOException {
 
         if(principal == null){
-            System.out.println("PRINCIPAL NULL");
-        }else{
-            System.out.println("getName() = "+ principal.getName());
-            System.out.println("sub = " + principal.getAttribute("sub"));
-
+            return ResponseEntity.status(401).body("Unauthorized");
         }
-
 
         customMetricsService.incrementTotalApiRequests();
         String ip = getClientIp(request);
         String key = "rate_limit:ip" + ip;
-        String id = session.getId();
+        String sessionId = principal.getAttribute("sub");
+
         System.out.println("LATITUDE"+transactionInfo.getLatitude());
         System.out.println("LONGITUDE"+transactionInfo.getLongitude());
 
-        if (session.isNew()) {
-            session.setAttribute("created", System.currentTimeMillis());
-        }
 
-        System.out.println("Transaction endpoint - Session ID: " + id);
+        System.out.println("Transaction endpoint - Session ID: " + sessionId);
 
         if (rateLimiting.isAllowed(key) && !transactionSecurityCheck.checkVpn(ip)) {
             transactionInfo.setClientIp(ip);
-            transactionInfo.setId(id);
+            transactionInfo.setId(sessionId);
             transactionInfo.setResult("Not a vpn");
+
             kafkaTemplate.send("out-transactions", transactionInfo.getId(), transactionInfo);
             kafkaTemplate.send("transactions",transactionInfo);
+
             return ResponseEntity.accepted().body("Transaction submitted");
         } else {
+
             transactionInfo.setResult("Is a vpn");
             kafkaTemplate.send("out-transactions", transactionInfo.getId(), transactionInfo);
             customMetricsService.incrementRateLimitedRequests();
@@ -93,26 +80,41 @@ public class TransactionController {
     }
 
     @GetMapping("/streams/results")
-    public SseEmitter streamResults(HttpSession session) {
+    public SseEmitter streamResults(@AuthenticationPrincipal OAuth2User principal) {
 
-        if (session.isNew()) {
-            session.setAttribute("created", System.currentTimeMillis());
+
+        if(principal == null){
+            return null;
+        }else {
+            String sessionId = principal.getAttribute("sub");
+            System.out.println("SSE endpoint - Session ID: " + sessionId);
+            return setupSse.streamResults(principal);
         }
 
-        String sessionId = session.getId();
-        System.out.println("SSE endpoint - Session ID: " + sessionId);
-        return setupSse.streamResults(session);
+
     }
 
     @GetMapping()
-    public Map<String, String> getSessionId(HttpSession session) {
-        if (session.isNew()) {
-            session.setAttribute("created", System.currentTimeMillis());
-        }
+    public ResponseEntity<Map<String, String>> getSessionId(@AuthenticationPrincipal OAuth2User principal, HttpServletResponse response) {
 
-        String sessionId = session.getId();
-        System.out.println("Session ID endpoint - Session ID: " + sessionId);
-        return Map.of("sessionId", sessionId);
+        if(principal == null){
+            return ResponseEntity.status(401).body(Map.of("Error","Unauthorized"));
+        }
+        String sessionId = principal.getAttribute("sub");
+
+        ResponseCookie cookie = ResponseCookie.from("sessionId", sessionId)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .sameSite("Lax")
+                .maxAge(3600)
+                .build();
+        response.addHeader("Set-Cookie",cookie.toString());
+        return ResponseEntity.ok(Map.of("sessionId",sessionId));
+
+
+
+
     }
 
 
