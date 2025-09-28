ae2125d02374cd154db54d5c414d5c842b948a12
Samoreilly <x23756575@student.ncirl.ie>
2025-09-28 13:47:09 +0100

    setting up dead letter queue

---
+++ src/main/java/fraud/fraud/DLQ/ViewDeadLetterQueue.java
@@ -0,0 +1,10 @@
+package fraud.fraud.DLQ;
+
+public class ViewDeadLetterQueue {
+    //THIS CLASS WILL BE USED TO PREVIEW DEAD LETTER QUEUE THROUGH AN ENDPOINT IN THE BROWSER
+
+
+    public void previewDLQ(){
+
+    }
+}
