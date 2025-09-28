4de952907169d85d3757d8bc25788f2bddee60ba
Samoreilly <x23756575@student.ncirl.ie>
2025-09-28 14:35:15 +0100

    Setting up Postgres db for Dead letter queue and other necessary classes

---
+++ src/main/java/fraud/fraud/DTO/DatabaseDTO.java
@@ -0,0 +1,88 @@
+package fraud.fraud.DTO;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+public class DatabaseDTO {
+
+    @Id
+    private String id;
+
+    private String data;
+    private LocalDateTime time;
+    private String clientIp;
+    private String result;
+    private Double latitude;
+    private Double longitude;
+    private int isFraud;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public void setClientIp(String clientIp) {
+        this.clientIp = clientIp;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getIsFraud() {
+        return isFraud;
+    }
+
+    public void setIsFraud(int isFraud) {
+        this.isFraud = isFraud;
+    }
+
+    public DatabaseDTO() {}
+
+
+}
---
+++ src/main/java/fraud/fraud/DTO/DatabaseRepo.java
@@ -0,0 +1,10 @@
+package fraud.fraud.DTO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DatabaseRepo extends JpaRepository<DatabaseDTO, Long> {
+
+
+}
