da5e030a5016fa68c3b3502f16a73e89918eb6ab
Samoreilly <x23756575@student.ncirl.ie>
2025-09-28 14:35:15 +0100

    Setting up Postgres db for Dead letter queue and other necessary classes

--- src/main/java/fraud/fraud/FraudDetectorApplication.java
+++ src/main/java/fraud/fraud/FraudDetectorApplication.java
@@ -2,20 +2,16 @@ package fraud.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.kafka.annotation.EnableKafka;
 
 
 @EnableCaching
 @EnableKafka
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class FraudDetectorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FraudDetectorApplication.class, args);
 
 	}
-
-
 }
